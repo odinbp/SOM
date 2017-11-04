30ccf2b442bb3a772f8c83e56fd93bcfd9646cde
vewo <vemundwo@gmail.com>
2017-11-04 23:24:21 +0100

    Funker

--- SOM.py
+++ SOM.py
@@ -15,6 +15,7 @@ class SOM(object):
 			self.size0 = max(m,n)/2.0
 		else:
 			self.size0 = float(size0)
+		self.size1 = size0
 		self.lr = self.lr0
 		self.size = self.size0
 		self.tlr = tlr
@@ -39,7 +40,7 @@ class SOM(object):
 		p = 2*size**2
 		if p == 0:
 			return 0
-		return math.exp(-distance**2/p)
+		return math.exp((-distance**2)/p)
 		
 
 	def som(self, neurons, inputs, iterations, k):
@@ -51,7 +52,7 @@ class SOM(object):
 				self.plot_map(inputs, neurons, i)
 			self.som_one_step(neurons, inputs, i)
 			
-	def som_one_step(self, neurons, inputs, i):
+	def som_one_step(self, neurons, inputs, iter):
 
 		#Pick a random input vector
 		iv = inputs[random.randint(0,len(inputs)-1)]
@@ -62,11 +63,12 @@ class SOM(object):
 		for neuronIndex, neuron in enumerate(neurons):
 			d = self.circle_distance(len(neurons), neuronIndex, winningIndex)
 			tn = self.neighbourhood(d, self.size)
+			print(self.size)
 			for i in range(len(neuron)):
 				neuron[i] += self.lr*tn*(iv[i]-neuron[i])
 
-		self.size_decay(i)
-		self.learning_decay(i)
+		self.size_decay(iter)
+		self.learning_decay(iter)
 
 	def find_winner(self, iv, neurons):
 		mi,mn = -100,[999999999999999.0,999999999999999.0]
@@ -83,7 +85,7 @@ class SOM(object):
 	def size_decay(self, t):
 		self.sizeChanges.append(self.size0*math.exp(-t/self.tsize)-self.size)
 		self.size = self.size0*math.exp(-t/self.tsize)
-
+		
 
 	def learning_decay(self, t):
 		self.lrateChanges.append(self.lr0*math.exp(-t/self.tlr)-self.lr)
@@ -126,14 +128,13 @@ class SOM(object):
 		plt.scatter(*zip(*neurons), color='green', s=5)
 		plt.plot(*zip(*(neurons+[neurons[0]])), color = 'blue')
 		plt.title('Iteration #{:06d}'.format(iteration))
-		plt.axis('off')
 		plt.draw()
-		plt.pause(0.01)
+		plt.pause(0.1)
 
 
 
 
-som = SOM(n_iterations=500,lr0 = 0.7, tlr = 0.999, size0 = 29*8/10, tsize = 0.995) 
+som = SOM(n_iterations=1000,lr0 = 0.7, tlr = 995, size0 = 29*8/10, tsize = 195) 
 som.main('test.txt')
 
 
