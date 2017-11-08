5f0dd7ace51c54f22612b3edeb91bc39df79b816
Odin Blomhoff Pedersen <odinblomhoff@gmail.com>
2017-11-08 22:02:10 +0100

    Added possibility to test given image.

--- SOMMnist.py
+++ SOMMnist.py
@@ -4,6 +4,27 @@ import csv
 import matplotlib.pyplot as plt
 import mnist_basics as mb
 
+testVector = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+   0, 0, 0, 0, 0, 0, 3, 18, 18, 18, 126, 136, 175, 26, 166, 255, 247, 127, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 30, 36, 94, 154, 170, 253, 253, 253, 253,
+    253, 225, 172, 253, 242, 195, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 49, 238, 253, 253, 253, 253, 253, 253, 253, 253, 251, 93, 82, 82, 56, 39, 0, 0, 0,
+     0, 0, 0, 0, 0, 0, 0, 0, 0, 18, 219, 253, 253, 253, 253, 253, 198, 182, 247, 241, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 80, 156, 107,
+      253, 253, 205, 11, 0, 43, 154, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 14, 1, 154, 253, 90, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 139, 253, 190, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11, 190, 253, 70, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 35, 241, 225, 160, 108, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 81, 240, 253, 253, 119, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 45, 186, 253, 253, 150, 27,
+          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16, 93, 252, 253, 187, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+           0, 0, 0, 0, 0, 0, 0, 249, 253, 249, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 46, 130, 183, 253, 253, 207, 2, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 39, 148, 229, 253, 253, 253, 250, 182, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 24,
+             114, 221, 253, 253, 253, 253, 201, 78, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 23, 66, 213, 253, 253, 253, 253, 198, 81, 2, 0, 0, 0,
+              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 18, 171, 219, 253, 253, 253, 253, 195, 80, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 55, 172, 226,
+               253, 253, 253, 253, 244, 133, 11, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 136, 253, 253, 253, 212, 135, 132, 16, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+testClass = 5
+
+
 class SOMMnist(object):
 
 	def __init__(self, n_iterations=100, lr0 = None, tlr = None, size0 = None, tsize = None, multiplier = 2):
@@ -38,8 +59,6 @@ class SOMMnist(object):
 
 	def discriminant(self, iv, weight):
 		d = 0
-		#print(len(iv[0]), "iv") #[[vector], class], må gjøres om til bare [vector]
-		#print(len(weight), "weight") #[x,y] -> wtf?
 		for i in range(len(iv)):
 			d +=  (iv[0][i]-weight[i])**2
 		return math.sqrt(d)
@@ -56,8 +75,6 @@ class SOMMnist(object):
 		#You need only compute total distance (D) and show diagrams at every k steps
 		#print(neurons, "som") #feil
 		for i in range(iterations+1):
-			#if i%k == 0:
-			#	self.plot_map(inputs, neurons, i)
 			self.som_one_step(neurons, inputs, i)
 			
 	def som_one_step(self, neurons, inputs, iter):
@@ -65,7 +82,6 @@ class SOMMnist(object):
 		#Pick a random input vector
 		iv = inputs[random.randint(0,len(inputs)-1)]
 		#Find winner of cometition
-		#print(neurons, "somonestep") #feil
 		winningIndex, winner = self.find_winner(iv, neurons[0])
 		neurons[1][winningIndex].append(iv[1])
 		iv = iv[0]
@@ -73,8 +89,6 @@ class SOMMnist(object):
 			d = self.grid_distance(neuronIndex, winningIndex)
 			tn = self.neighbourhood(d, self.size)
 			for i in range(len(neuron)):
-				#print(neuron[i], "neuron[i]")
-				#print(iv[i], "iv[i]")
 				neuron[i] += self.lr*tn*(iv[i]-neuron[i])
 
 		self.size_decay(iter)
@@ -95,9 +109,9 @@ class SOMMnist(object):
 		winningIndex = str(winningIndex)
 
 		if len(neuronIndex) == 1:
-			neuronIndex+= '0'
+			neuronIndex = '0'+ neuronIndex
 		if len(winningIndex) == 1:
-			winningIndex+='0'
+			winningIndex = '0'+ winningIndex
 
 		NIX = int(neuronIndex[0])
 		NIY = int(neuronIndex[1])
@@ -133,7 +147,7 @@ class SOMMnist(object):
 		return scale,data
 
 	def main(self):
-		noOfImages, images = self.read_data(100)
+		noOfImages, images = self.read_data(50000)
 		print("Done with loading")
 		scaleFactor, scaled = self.normalize(images)
 		#print(len(images[0])) == 2
@@ -154,11 +168,20 @@ class SOMMnist(object):
 
 				average.append(round(total/lengthOfList))
 
+		
+		a, b = self.find_winner([testVector, testClass], neurons[0])
+		print(average[a])
+
 		#return neurons[1]
 		return average
 
-som = SOMMnist(n_iterations=2000,lr0 = 0.7, tlr = 1000, size0 = 70/10, tsize = 200, multiplier = 10) 
-print(som.main())
+som = SOMMnist(n_iterations=1000,lr0 = 1, tlr = 1000, size0 = 70/10, tsize = 200, multiplier = 10) 
+grid = som.main()
 
-
-
+index = 0
+for i in range(10):
+	lal = []
+	for j in range(10):
+		lal.append(grid[index])
+		index += 1
+	print(lal)
