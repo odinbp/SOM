63f59ba4dcff2aa70f016e71bb5f8e0af8540d8f
vewo <vemundwo@gmail.com>
2017-11-10 14:37:19 +0100

    Masse greier

--- SOM_tester.py
+++ SOM_tester.py
@@ -49,8 +49,8 @@ class SOM(object):
 		#You need only compute total distance (D) and show diagrams at every k steps
 		
 		for i in range(iterations+1):
-			if i%k == 0:
-				self.plot_map(inputs, neurons, i)
+			#if i%k == 0:
+			#	self.plot_map(inputs, neurons, i)
 			self.som_one_step(neurons, inputs, i)
 			
 	def som_one_step(self, neurons, inputs, iter):
@@ -96,11 +96,16 @@ class SOM(object):
 		scaleFactor, scaled = self.normalize(cities)
 		neurons = self.init_neurons(len(cities[0]), noCities)
 		self.som(neurons = neurons, inputs = scaled, iterations = self.n_iterations, k = 100)
+		#(print(self.calculate_distance(scaled, neurons)*scaleFactor))
 		coordinates, distance = self.calculate_distance(scaled, neurons)
 		distance *= scaleFactor
-		plt.close()
-		self.plot_finished(coordinates, distance)
+		#plt.close()
+		#self.plot_finished(coordinates, distance)
 		return distance
+
+		#for i in range(len(neurons)):
+		#	if (i-2)%5 == 0:
+		#		print(neurons[i][0]*scaleFactor[0], neurons[i][1]*scaleFactor[1])
 
 	def read_data(self, file):
 		cities = []
@@ -178,18 +183,34 @@ class SOM(object):
 
 noCi = {1:52, 2:130, 3:101, 4:100, 5:105, 6:76, 7:124, 8:99}
 
+def testing():
+	f = open('workfile2.txt', 'w')
+	for i in range(1,6):
+		for n in range(1,6):
+			d = 0
+			best = 99999
+			worst = 0
+			for o in range(5):
+				som = SOM(n_iterations=3000,lr0 = 0.7, tlr = i*500, size0 = noCi[1]/10, tsize = 200*n, multiplier = 8) 
+				r = som.main('1.txt')
+				d += r
+				if r < best:
+					best = r
+				if r > worst: 
+					worst = r
+			d = d/5
+			f.write("File: " + str(i) + "	|	AVG/B/W = " + str(round(d,2)) +"/" + str(round(best,2)) + "/" + str(round(worst,2)) + "	|	lr0 = " +  str(0.7) + ", tlr = " + str(i*500) + ", size0 = " + str(0.1) + ", tsize = " + str(200*n) + ", multiplier = " + str(8) + '\n')
+
+
+testing()
+
+
 #som = SOM(n_iterations=4000,lr0 = 0.5, tlr = 800, size0 = noCi[1]/10, tsize = 500, multiplier = 8) 
-'''
-d = 0
-for i in range(10):
-	som = SOM(n_iterations= 3000,lr0 = 0.7, tlr = 2000, size0 = noCi[8]/10, tsize = 500, multiplier = 8) 
-	k = som.main('8.txt')
-	d += k
-	print(k)
-print("Avg: ", d/10)
-'''
-som = SOM(n_iterations= 3000,lr0 = 0.7, tlr = 2000, size0 = noCi[1]/10, tsize = 500, multiplier = 8) 
-print(som.main('1.txt'))
+
+#som = SOM(n_iterations=2000,lr0 = 0.7, tlr = 1000, size0 = noCi[1]/10, tsize = 200, multiplier = 8) 
+#print(som.main('1.txt'))
+
+
 
 
 
