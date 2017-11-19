4fed13df3c80c9c5a3e574f6f3776bd833c9ece5
Odin Blomhoff Pedersen <odinblomhoff@gmail.com>
2017-11-19 13:27:05 +0100

    Added specify paramters.

--- SOM.py
+++ SOM.py
@@ -27,7 +27,24 @@ class SOM(object):
 				print("MNIST is chosen")
 				print("What's your desired k?")
 				k = int(input())
-				sommnist = SOMMnist(n_iterations=5000,lr0 = 0.3, tlr = 2000, size0 = 6, tsize = 800, noOfNeuron = 20, k = k)
+				print("Press 1 for optimized parameters and 2 to specify")
+				l = int(input())
+				if l == 1:
+					sommnist = SOMMnist(n_iterations=5000,lr0 = 0.3, tlr = 2000, size0 = 6, tsize = 800, noOfNeuron = 20, k = k)
+				elif l == 2:
+					print("Number of iterations?")
+					n_iterations = int(input())
+					print("Learning rate?")
+					lr0 = float(input())
+					print("Learning rate Tau")
+					tlr = float(input())
+					print("Neighbourhood Size?")
+					size0 = int(input())
+					print("Size Tau rate?")
+					tsize = int(input())
+					print("Number of Neurons?")
+					noOfNeuron = int(input())
+					sommnist = SOMMnist(n_iterations=n_iterations,lr0 = lr0, tlr = tlr, size0 = size0, tsize = tsize, noOfNeuron = noOfNeuron, k = k)
 				sommnist.main()
 
 			elif (ans == 1):
